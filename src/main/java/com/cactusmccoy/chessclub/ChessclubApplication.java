f7879be223beac29ff81052e686f967bd63d2a7d
GamboaJavier <danielgba58@gmail.com>
2026-07-18 17:25:03 -0400

    feat: Starting a project by setting up the folder architecture with Spring Initializr.

---
+++ src/main/java/com/cactusmccoy/chessclub/ChessclubApplication.java
@@ -0,0 +1,13 @@
+package com.cactusmccoy.chessclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cactusmccoy/chessclub/ChessclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cactusmccoy.chessclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
