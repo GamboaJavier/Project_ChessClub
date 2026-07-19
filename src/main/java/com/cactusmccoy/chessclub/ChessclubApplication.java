4cc049b161cbaf34b0478a2cf9b5fbf0ef7cfd44
Yosnel Guerra <124190702+Ysaidd@users.noreply.github.com>
2026-07-18 22:05:19 -0400

    fix java class for merge

--- src/main/java/com/cactusmccoy/chessclub/ChessclubApplication.java
+++ src/main/java/com/cactusmccoy/chessclub/ChessclubApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChessclubApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChessclubApplicatsssion.class, args);
+		SpringApplication.run(ChessclubApplication.class, args);
 	}
 
 }
